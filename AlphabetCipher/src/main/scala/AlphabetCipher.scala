804f0212ef9e12653c27fd15350e58efb6b83a9a
Mario Rodriguez <mario.rodriguez@jambit.com>
2018-02-06 18:19:30 +0100

    Solution to AlphabetCipher

--- AlphabetCipher/src/main/scala/AlphabetCipher.scala
+++ AlphabetCipher/src/main/scala/AlphabetCipher.scala
@@ -1,14 +1,105 @@
+import scala.annotation.tailrec
+
 object AlphabetCipher {
+  val Alphabet: Vector[Char] = "abcdefghijklmnopqrstuvwxyz".toVector
+
+  @tailrec private def rotateRecursive[T](acc: Vector[T], i: Int): Vector[T] = {
+    if (i == 0) acc
+    else rotateRecursive(acc.tail :+ acc.head, i - 1)
+  }
+
+  def rotate[T](list: Vector[T], n: Int = 1): Vector[T] = {
+    rotateRecursive(list, n)
+  }
+
+  @tailrec def recursiveEncode(keyword: String,
+                               message: String,
+                               messagePos: Int,
+                               acc: String): String = {
+    if (messagePos == message.length) acc
+    else {
+      val rotations = Alphabet.indexOf(message.charAt(messagePos))
+      val rotatedAlphabet = rotate(Alphabet, rotations)
+      val resultChar = rotatedAlphabet(
+        Alphabet.indexOf(keyword.charAt(messagePos)))
+      recursiveEncode(keyword, message, messagePos + 1, acc + resultChar)
+    }
+  }
+
+  @tailrec def recursiveDecodeRotations(keywordChar: Char,
+                                        messageChar: Char,
+                                        iter: Int): Int = {
+    val rotatedAlphabet = rotate(Alphabet, iter)
+    val index = Alphabet.indexOf(keywordChar)
+    val char = rotatedAlphabet.charAt(index)
+    if (messageChar == char) iter
+    else recursiveDecodeRotations(keywordChar, messageChar, iter + 1)
+  }
+
+  def decodeRotations(keywordChar: Char, messageChar: Char) =
+    recursiveDecodeRotations(keywordChar, messageChar, 0)
+
+  @tailrec def recursiveDecode(keyword: String,
+                               message: String,
+                               messagePos: Int,
+                               acc: String): String = {
+    if (messagePos == message.length) acc
+    else {
+      val rotations =
+        decodeRotations(keyword.charAt(messagePos), message.charAt(messagePos))
+      val resultChar = Alphabet.charAt(rotations)
+      recursiveDecode(keyword, message, messagePos + 1, acc + resultChar)
+    }
+  }
+
+  @tailrec def recursiveDecipher(cipher: String,
+                                 message: String,
+                                 i: Int,
+                                 acc: String): String = {
+    if (i == message.length) acc
+    else {
+      val rotations =
+        decodeRotations(cipher.charAt(i), message.charAt(i))
+      val resultChar = Alphabet.charAt(rotations)
+      recursiveDecipher(cipher, message, i + 1, acc + resultChar)
+    }
+  }
+
+  @tailrec def enlargeKeyword(keyword: String,
+                              message: String,
+                              acc: String): String = {
+    if (acc.length > message.length) acc.dropRight(acc.length - message.length)
+    else enlargeKeyword(keyword, message, acc + keyword)
+  }
+
+  @tailrec def findRepeatingWord(keywordManyTimes: String,
+                                 acc: String,
+                                 pos: Int): String = {
+    if (pos == keywordManyTimes.length)
+      acc
+    else if (acc.length + pos - 1 >= keywordManyTimes.length)
+      keywordManyTimes
+    else if (acc == keywordManyTimes.substring(acc.length, acc.length + pos))
+      acc
+    else
+      findRepeatingWord(keywordManyTimes,
+                        acc + keywordManyTimes.charAt(pos).toString,
+                        pos + 1)
+  }
+
   def encode(keyword: String, message: String): String = {
-    ??? //keyword
+    val enlargedKeyword = enlargeKeyword(keyword, message, keyword)
+    recursiveEncode(enlargedKeyword, message, 0, "")
   }
 
   def decode(keyword: String, message: String): String = {
-    ??? //keyword
+    val enlargedKeyword = enlargeKeyword(keyword, message, keyword)
+    recursiveDecode(enlargedKeyword, message, 0, "")
   }
 
   def decipher(cipher: String, message: String): String = {
-    ??? //cipher
+    val keywordManyTimes = recursiveDecipher(message, cipher, 0, "")
+    findRepeatingWord(keywordManyTimes, keywordManyTimes.charAt(0).toString, 1)
   }
 
 }
--- AlphabetCipher/src/test/scala/AlphabetCipherSpec.scala
+++ AlphabetCipher/src/test/scala/AlphabetCipherSpec.scala
@@ -3,18 +3,40 @@ import org.scalatest._
 
 class AlphabetCipherSpec extends FlatSpec with Matchers {
   "encode" should "encode given a secret keyword" in {
-    assert(encode("vigilance","meetmeontuesdayeveningatseven") == "hmkbxebpxpmyllyrxiiqtoltfgzzv")
-    assert(encode("scones","meetmebythetree") == "egsgqwtahuiljgs")
+    assert(encode("bcd", "meet") == "nghu")
+    assert(
+      encode("vigilance", "meetmeontuesdayeveningatseven") == "hmkbxebpxpmyllyrxiiqtoltfgzzv")
+    assert(encode("scones", "meetmebythetree") == "egsgqwtahuiljgs")
   }
 
-  "decode" should "decode an cyrpted message given a secret keyword" in {
-    assert(decode("vigilance","hmkbxebpxpmyllyrxiiqtoltfgzzv") == "meetmeontuesdayeveningatseven")
-    assert(decode("scones","egsgqwtahuiljgs") == "meetmebythetree")
+  "decode" should "decode an encyrpted message given a secret keyword" in {
+    assert(decode("bcd", "nghu") == "meet")
+    assert(
+      decode("vigilance", "hmkbxebpxpmyllyrxiiqtoltfgzzv") == "meetmeontuesdayeveningatseven")
+    assert(decode("scones", "egsgqwtahuiljgs") == "meetmebythetree")
 
   }
 
   "decipher" should "extract the secret keyword given an encrypted message and the original message" in {
-    assert(decipher("opkyfipmfmwcvqoklyhxywgeecpvhelzg", "thequickbrownfoxjumpsoveralazydog") == "vigilance")
-    assert(decipher("hcqxqqtqljmlzhwiivgbsapaiwcenmyu", "packmyboxwithfivedozenliquorjugs") == "scones")
+    assert(decipher("hmkbxebpxpmyllyrxiiqtoltfgzzv",
+                    "meetmeontuesdayeveningatseven") == "vigilance")
+    assert(
+      decipher("opkyfipmfmwcvqoklyhxywgeecpvhelzg",
+               "thequickbrownfoxjumpsoveralazydog") == "vigilance")
+    assert(
+      decipher("hcqxqqtqljmlzhwiivgbsapaiwcenmyu",
+               "packmyboxwithfivedozenliquorjugs") == "scones")
   }
+
+  "rotate" should "rotate things" in {
+    assert(rotate("abc".toVector, 1) == "bca".toVector)
+    assert(rotate("abc".toVector, 3) == "abc".toVector)
+  }
+
+  "findRepeatingWord" should "find repeating words" in {
+    assert((findRepeatingWord("abcabc", "a", 1) == "abc"))
+    assert((findRepeatingWord("abc", "a", 1) == "abc"))
+    assert((findRepeatingWord("abcabcab", "a", 1) == "abc"))
+  }
+
 }
